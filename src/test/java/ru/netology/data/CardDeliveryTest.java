b74dd6f79dad363d515fdd8bb45d59877426c4f2
dmitry089 <dmitry.karaulov@rambler.ru>
2023-05-09 12:22:52 +0400

    change parametr test

--- src/test/java/ru/netology/data/CardDeliveryTest.java
+++ src/test/java/ru/netology/data/CardDeliveryTest.java
@@ -1,8 +1,8 @@
 package ru.netology.data;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -11,7 +11,14 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.visible;
 class CardDeliveryTest {
-
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
